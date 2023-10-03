8039b3b03bd63f505f9ff149b6dabb1c67856199
akolomytsev <akolomytsev@ptsecurity.com>
2023-10-04 00:39:57 +0300

    add contact test with class

---
+++ contact.py
@@ -0,0 +1,34 @@
+class Contact:
+
+    def __init__(self, firstname="firstname", middlename="middlename", lastname="lastname", nickname="nickname",
+                 photo="C:\\Users\\akolomytsev\\Desktop\\test.jpg", title="title", company="company", address="address",
+                 homephone="homephone", mobile="mobile", workphone="workphone", fax="fax",
+                 email="email", email2="email2", email3="email3", homepage="homepage", bday="1", bmonth="January",
+                 byear="2024", aday="1", amonth="January", ayear="2024", address2="address2",
+                 homephone2="homephone2", notes="notes"):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.mobile = mobile
+        self.workphone = workphone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.homephone2 = homephone2
+        self.notes = notes
+
