b59b7e96e51295916fa57836d89e17496e7e2329
akolomytsev <akolomytsev@ptsecurity.com>
2023-11-17 17:26:18 +0300

    optimize pages navigation

--- fixture/group.py
+++ fixture/group.py
@@ -5,7 +5,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element("link text", "groups").click()
+        if not (wd.current_url.endswith("/groups.php") and len(wd.find_elements("name", "new")) > 0):
+            wd.find_element("link text", "groups").click()
 
     def create(self, group):
         wd = self.app.wd
