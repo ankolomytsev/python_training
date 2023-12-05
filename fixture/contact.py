e5eecdf5809d4a9ed43f72407806afb5a1619a9a
akolomytsev <akolomytsev@ptsecurity.com>
2023-12-05 20:02:34 +0300

    add caching and hashing for contact list

--- fixture/contact.py
+++ fixture/contact.py
@@ -12,6 +12,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         wd.find_element("xpath", "//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
+        self.contact_cache = None
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -20,6 +21,7 @@ class ContactHelper:
         alert = wd.switch_to.alert
         alert.accept()
         self.return_to_home_page()
+        self.contact_cache = None
 
     def edit_first_contact(self, new_contact_data):
         wd = self.app.wd
@@ -27,6 +29,7 @@ class ContactHelper:
         self.fill_contact_form(new_contact_data)
         wd.find_element("name", "update").click()
         self.return_to_home_page()
+        self.contact_cache = None
 
     def fill_contact_form(self, contact):
         self.change_text_field_value("firstname", contact.firstname)
@@ -76,13 +79,16 @@ class ContactHelper:
         self.app.open_home_page()
         return len(wd.find_elements("name", "selected[]"))
 
+    contact_cache = None
+
     def get_contact_list(self):
-        wd = self.app.wd
-        self.app.open_home_page()
-        contacts = []
-        for element in wd.find_elements("name", "entry"):
-            id = element.find_element("name", "selected[]").get_attribute("value")
-            firstname = element.find_element("xpath", "td[3]").text
-            lastname = element.find_element("xpath", "td[2]").text
-            contacts.append(Contact(firstname=firstname, lastname=lastname, id=id))
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.app.open_home_page()
+            self.contact_cache = []
+            for element in wd.find_elements("name", "entry"):
+                id = element.find_element("name", "selected[]").get_attribute("value")
+                firstname = element.find_element("xpath", "td[3]").text
+                lastname = element.find_element("xpath", "td[2]").text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+        return list(self.contact_cache)
